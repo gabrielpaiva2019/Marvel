dcdb14cee547b3b03e01c7ce96dcb34b106b8eb3
cgpaiva <cgpaiva@icloud.com>
2021-10-04 18:47:15 -0300

    Inicia estrutura de pastas

---
+++ app/src/main/java/com/paiva/marvel/screens/heroesList/HeroesActivity.kt
@@ -0,0 +1,12 @@
+package com.paiva.marvel.screens.heroesList
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.paiva.marvel.R
+
+class HeroesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

25d0659c046eae76c8308528c15c89ff861f18d2
cgpaiva <cgpaiva@icloud.com>
2021-10-04 23:59:32 -0300

    Limpeza de código

--- app/src/main/java/com/paiva/marvel/infra/CustomApplication.kt
+++ app/src/main/java/com/paiva/marvel/infra/CustomApplication.kt
@@ -3,7 +3,7 @@ package com.paiva.marvel.infra
 import android.app.Application
 import com.paiva.marvel.infra.koin.apiModule
 import com.paiva.marvel.infra.koin.heroesModules
-import com.paiva.marvel.infra.koin.netModule
+import com.paiva.marvel.infra.koin.connectionModule
 import com.paiva.marvel.infra.koin.serviceModules
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
@@ -16,7 +16,7 @@ class CustomApplication: Application() {
         startKoin {
             androidLogger(Level.DEBUG)
             androidContext(this@CustomApplication)
-            modules(listOf(heroesModules, apiModule, netModule, serviceModules))
+            modules(listOf(heroesModules, apiModule, connectionModule, serviceModules))
         }
     }
 }
--- app/src/main/java/com/paiva/marvel/infra/koin/ApplicationModules.kt
+++ app/src/main/java/com/paiva/marvel/infra/koin/ApplicationModules.kt
@@ -1,19 +1,10 @@
 package com.paiva.marvel.infra.koin
 
-import android.app.Application
-import android.content.Context
-import android.net.ConnectivityManager
-import android.net.NetworkInfo
-import com.google.gson.FieldNamingPolicy
-import com.google.gson.Gson
-import com.google.gson.GsonBuilder
 import com.paiva.marvel.BuildConfig
 import com.paiva.marvel.api.MarvelApi
 import com.paiva.marvel.screens.heroesList.HeroesViewModel
 import com.paiva.marvel.service.MarvelService
-import okhttp3.Cache
 import okhttp3.OkHttpClient
-import org.koin.android.ext.koin.androidApplication
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 import retrofit2.Retrofit
@@ -35,8 +26,7 @@ val apiModule = module {
     single { provideUserApi(get()) }
 }
 
-val netModule = module {
-
+val connectionModule = module {
 
     fun provideHttpClient(): OkHttpClient {
         val okHttpClientBuilder = OkHttpClient.Builder()
@@ -44,24 +34,14 @@ val netModule = module {
         return okHttpClientBuilder.build()
     }
 
-
-
-    fun provideGson(): Gson {
-        return GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.IDENTITY).create()
-    }
-
-
-    fun provideRetrofit(factory: Gson, client: OkHttpClient): Retrofit {
+    fun provideRetrofit(): Retrofit {
         return Retrofit.Builder()
             .baseUrl(BuildConfig.GATEWAY_BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
 
-
-
     single { provideHttpClient() }
-    single { provideGson() }
-    single { provideRetrofit(get(), get()) }
+    single { provideRetrofit() }
 
 }
